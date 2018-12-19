c11ac02b30a3e569d9f5feac424f642d6a9c96df
amanjmd <amanjmd100@gmail.com>
2018-12-19 13:58:51 +0530

    First Commit

---
+++ src/CustomList.java
@@ -0,0 +1,12 @@
+
+public interface CustomList {
+
+	public CustomListNode addNode(int pData);
+	public CustomListNode getHeadNode();
+	public void deleteNodeAt(int pPosition);
+	public void SearchNode(CustomListNode pNewNode);
+	public CustomListNode getNodeFromEnd(int pPosition);
+	public boolean isCyclicList();
+	public CustomListNode getStartOfCycle();
+	
+}
---
+++ src/CustomListNode.java
@@ -0,0 +1,17 @@
+
+public class CustomListNode {
+
+	public int data;
+	public CustomListNode nextNode;
+	public CustomListNode(int data) {
+
+		this.data=data;
+	}
+	
+	@Override
+	public String toString() {
+		
+		return this.data+"";
+	}
+	
+}
